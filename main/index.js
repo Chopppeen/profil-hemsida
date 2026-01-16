3cd5730cab1ea4942ce5cdcc42443fb9ea6e1575
Chopppeen <sam09buster@gmail.com>
2026-01-16 10:11:29 +0000

    [ADD] provade att ändra text när man tröck på dark mode, funkar

--- main/index.js
+++ main/index.js
@@ -8,5 +8,10 @@ toggleButton.addEventListener('click', () => {
     header.classList.toggle('dark-mode');
     nav.classList.toggle('dark-mode');
     footer.classList.toggle('dark-mode');
-});
 
+/*jag trodde ärligt att det här inte skulle funka men W*/
+if (document.body.classList.contains('dark-mode')) {
+    toggleButton.textContent = '☀️Ljust läge';
+} else {
+    toggleButton.textContent = '🌙Mörkt läge';
+}});
